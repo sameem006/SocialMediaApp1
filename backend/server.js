5d8c56862a649cb5882fe35b7f25be3edc51a88b
sameem <mohamedsameem300@gmail.com>
2026-08-16 21:57:14 +0530

    Modified serveri.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use("/", (req, res) => {
+      res.send("hi");
+});
+
+app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+});
