1d99d228ba991a242712304c178930d932aa6dfd
sameem <mohamedsameem300@gmail.com>
2026-08-18 20:13:39 +0530

    Fix bug in connectDB

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dns from "dns";
 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const ConnectDB = async () => {
       try {
             await mongoose.connect(process.env.MONGODB_URL);
