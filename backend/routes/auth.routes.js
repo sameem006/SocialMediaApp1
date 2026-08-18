c17e4c9306532c26ec0a48a2952fe7e80daebbcc
sameem <mohamedsameem300@gmail.com>
2026-08-18 21:06:10 +0530

    Add logout route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, signUp } from "../controllers/authController.js";
+import { login, logout, signUp } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.post("/logout", logout);
 export default router;
