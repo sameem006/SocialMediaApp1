ce021404903de4d8f9b2b97c257393d791308986
sameem <mohamedsameem300@gmail.com>
2026-08-18 20:25:59 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import ConnectDB from "./db/connectDB.js";
 import authRoute from "./routes/auth.routes.js";
+import cors from "cors";
 const app = express();
 
 dotenv.config();
-express.json();
+
 const PORT = process.env.PORT;
 
-app.use("api/auth", authRoute);
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
       console.log(`Server is running on PORT ${PORT}`);
