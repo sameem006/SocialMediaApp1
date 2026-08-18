ee567d6ecb9e2052ff8b3f07ed9b235a1bb75d11
sameem <mohamedsameem300@gmail.com>
2026-08-18 20:53:10 +0530

    Add loginRoute

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { login, signUp } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
-
+router.post("/login", login);
 export default router;
