075a39378f176e1f6ca8b848933591cc63872b7a
sameem <mohamedsameem300@gmail.com>
2026-08-18 20:15:30 +0530

    Modified authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,48 @@
+import User from "../model/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signUp = async (req, res) => {
       try {
-      } catch (error) {}
+            const { username, fullName, email, password } = req.body;
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!emailRegex.test(email)) {
+                  return res.status(400).json({ error: "invalid email format" });
+            }
+
+            const existingEmail = await User.findOne({ email });
+            const existingUsername = await User.findOne({ username });
+
+            if (existingEmail || existingUsername) {
+                  return res.status(400).json({ error: "email or username already exist" });
+            }
+
+            if (password.length < 6) {
+                  return res.status(400).json({ error: "password must have atleast 6 chars" });
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const newUser = new User({
+                  username,
+                  email,
+                  fullName,
+                  password: hashedPassword,
+            });
+
+            if (newUser) {
+                  await newUser.save();
+                  res.status(200).json({
+                        _id: newUser._id,
+                        username: newUser.username,
+                        email: newUser.email,
+                        fullName: newUser.fullName,
+                  });
+            } else {
+                  res.status(400).json({ error: "invalid user data" });
+            }
+      } catch (error) {
+            console.log(`error in signup controller: ${err}`);
+            res.status(500).json("internal server error");
+      }
 };
